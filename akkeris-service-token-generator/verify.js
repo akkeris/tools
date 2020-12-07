5c1fae081a0c75e24964c4b065e7ef45d6cffc25
Sam Beckett <sbeck14@me.com>
2020-12-07 12:35:11 -0700

    remove unneeded require

--- akkeris-service-token-generator/verify.js
+++ akkeris-service-token-generator/verify.js
@@ -1,6 +1,5 @@
 const assert = require('assert');
 const { jwks_verify } = require('./lib');
-const jose = require('node-jose');
 
 assert.ok(process.env.JWT_PRIVATE_KEY, 'The environment variable JWT_PRIVATE_KEY was not found.');
 assert.ok(process.env.AKKERIS_API, 'The environment variable AKKERIS_API was not found');
